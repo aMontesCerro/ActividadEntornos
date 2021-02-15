478af2cd03790961f6be67bde4d287e9355a492e
aMontesCerro <78167220+aMontesCerro@users.noreply.github.com>
2021-02-15 16:14:25 +0100

    Agregado javadoc

---
+++ Cociente.java
@@ -0,0 +1,65 @@
+
+/**
+ * Esta clase implementa todos los métodos que están relacionados con la
+ * división de la calculadora
+ * 
+ * 
+ * @author Álvaro Montes Cerro
+ *
+ */
+
+public class Cociente {
+	/**
+	 * Realiza el cociente entre dos numeros reales pasados como parámetro La
+	 * pantalla mostrará "Error" si el num2 introducido es 0
+	 * 
+	 * @param num1 Dividendo del cociente
+	 * @param num2 Divisor del cociente
+	 * @return Devuelve el cociente de num1 entre num2
+	 * @exception ArithmeticException Si num2 = 0
+	 */
+
+	public double cocienteReales(double num1, double num2) {
+
+		return 0;
+	}
+
+	/**
+	 * Realiza el cociente entre dos numeros enteros pasados como parámetro La
+	 * pantalla mostrará "Error" si el num2 introducido es 0
+	 * 
+	 * @param num1 Dividendo del cociente
+	 * @param num2 Divisor del cociente
+	 * @return Devuelve el resultado de dividir num1 entre num2
+	 * @exception ArithmeticException Si num2 = 0
+	 */
+	public double cocienteEnteros(int num1, int num2) {
+
+		return 0;
+	}
+
+	/**
+	 * Calcula el número inverso de un número real pasado por parámetro La pantalla
+	 * mostrará "Error" si el número introducido es 0
+	 * 
+	 * @param num1 Número del que se quiere calcular el inverso
+	 * @return Devuelve un real que es el número inverso de num1
+	 * @exception ArithmeticException si num1 = 0
+	 */
+
+	public double calculaInverso(double num1) {
+		return 0;
+	}
+
+	/**
+	 * Calcula la raiz cuadrada del número pasado como parámetro. La pantalla
+	 * mostrará "Error" si el número introducido es negativo
+	 * 
+	 * @param num1 Radicando
+	 * @return Devuelve un real que es la raiz cuadrada positiva de num1
+	 */
+	public double calculaRaiz(double num1) {
+		return 0;
+	}
+
+}
