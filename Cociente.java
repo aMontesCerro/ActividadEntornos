7c36bbe6356062ed84fa1caca65b218cb40dd572
aMontesCerro <78167220+aMontesCerro@users.noreply.github.com>
2021-02-19 12:36:42 +0100

    Corregido Javadoc

--- Cociente.java
+++ Cociente.java
@@ -10,8 +10,23 @@
 
 public class Cociente {
 	/**
-	 * Realiza el cociente entre dos numeros reales pasados como parámetro La
-	 * pantalla mostrará "Error" si el num2 introducido es 0
+	 * Realiza el cociente entre dos numeros reales pasados como parámetro.
+	 * <p>
+	 * Casos especiales:
+	 * </p>
+	 * 
+	 * <ul>
+	 * <li>Si el divisor introducido es un 0 dará error
+	 * <li>Si el divisor es 1 el resultado será el primer número
+	 * <li>Si el dividendo es 0, el resultado será 0
+	 * <li>Si el dividendo es infinito, devuelve infinito
+	 * <li>Si el divisor es infinito, devuelve 0
+	 * <li>Si ambos números son infinito, devuelve NaN
+	 * <li>Si el dividendo es infinito negativo, devuelve infinito negativo
+	 * <li>Si el divisor es infinito negativo, el resultado es infinito negativo
+	 * <li>Si ambos números son infinito negativo, devuelve NaN
+	 * <li>Si alguno de los números es NaN, el resultado será NaN
+	 * </ul>
 	 * 
 	 * @param num1 Dividendo del cociente
 	 * @param num2 Divisor del cociente
@@ -25,8 +40,23 @@ public class Cociente {
 	}
 
 	/**
-	 * Realiza el cociente entre dos numeros enteros pasados como parámetro La
-	 * pantalla mostrará "Error" si el num2 introducido es 0
+	 * Realiza el cociente entre dos numeros enteros pasados como parámetro .
+	 * <p>
+	 * Casos especiales:
+	 * </p>
+	 * 
+	 * <ul>
+	 * <li>Si el divisor introducido es un 0 dará error
+	 * <li>Si el divisor es 1 el resultado será el primer número
+	 * <li>Si el dividendo es 0, el resultado será 0
+	 * <li>Si el dividendo es infinito, devuelve infinito
+	 * <li>Si el divisor es infinito, devuelve 0
+	 * <li>Si ambos números son infinito, devuelve NaN
+	 * <li>Si el dividendo es infinito negativo, devuelve infinito negativo
+	 * <li>Si el divisor es infinito negativo, el resultado es infinito negativo
+	 * <li>Si ambos números son infinito negativo, devuelve NaN
+	 * <li>Si alguno de los números es NaN, el resultado será NaN
+	 * </ul>
 	 * 
 	 * @param num1 Dividendo del cociente
 	 * @param num2 Divisor del cociente
@@ -39,8 +69,17 @@ public class Cociente {
 	}
 
 	/**
-	 * Calcula el número inverso de un número real pasado por parámetro La pantalla
-	 * mostrará "Error" si el número introducido es 0
+	 * Calcula el número inverso de un número real pasado por parámetro.
+	 * <p>
+	 * Casos especiales;
+	 * </p>
+	 * 
+	 * <ul>
+	 * <li>Si el número introducido es NaN, devuelve NaN
+	 * <li>Si el numero introducido es infinito, devuelve 0
+	 * <li>Si el número es infinito negativo, deuelve -0
+	 * <li>Si el número introducido es 0, salta la excepción
+	 * </ul>
 	 * 
 	 * @param num1 Número del que se quiere calcular el inverso
 	 * @return Devuelve un real que es el número inverso de num1
@@ -52,8 +91,18 @@ public class Cociente {
 	}
 
 	/**
-	 * Calcula la raiz cuadrada del número pasado como parámetro. La pantalla
-	 * mostrará "Error" si el número introducido es negativo
+	 * Calcula la raiz cuadrada del número pasado como parámetro.
+	 * <p>
+	 * Casos especiales
+	 * </p>
+	 * 
+	 * <ul>
+	 * <li>Si el número introducido es negativo, devuelve NaN
+	 * <li>Si el radicando es 0, el resultado es 0
+	 * <li>Si el radicando es infinito, el resultado es infinito
+	 * <li>Si el radicando es infinito negativo, el resultado es NaN
+	 * <li>Si el radicando es NaN, el resultado es NaN
+	 * </ul>
 	 * 
 	 * @param num1 Radicando
 	 * @return Devuelve un real que es la raiz cuadrada positiva de num1
