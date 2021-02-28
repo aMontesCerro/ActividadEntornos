ce49b1cfa969cdfc0d1b9e9360ad8c427eeac0d8
aMontesCerro <78167220+aMontesCerro@users.noreply.github.com>
2021-02-28 18:37:06 +0100

    Corrigiendo errores

--- Cociente.java
+++ Cociente.java
@@ -9,6 +9,8 @@
  */
 
 public class Cociente {
+	private double resultado;
+
 	/**
 	 * Realiza el cociente entre dos numeros reales pasados como parámetro.
 	 * <p>
@@ -16,7 +18,7 @@ public class Cociente {
 	 * </p>
 	 * 
 	 * <ul>
-	 * <li>Si el divisor introducido es un 0 dará error
+	 * <li>Si el divisor introducido es un 0 devuelve NaN
 	 * <li>Si el divisor es 1 el resultado será el primer número
 	 * <li>Si el dividendo es 0, el resultado será 0
 	 * <li>Si el dividendo es infinito, devuelve infinito
@@ -31,14 +33,14 @@ public class Cociente {
 	 * @param num1 Dividendo del cociente
 	 * @param num2 Divisor del cociente
 	 * @return Devuelve el cociente de num1 entre num2
-	 * @exception ArithmeticException Si num2 = 0
+	 * 
 	 */
 
-	public double cocienteReales(double num1, double num2) throws ArithmeticException {
-
-		double resultado = num1 / num2;
-
-		return resultado;
+	public double cocienteReales(double num1, double num2) {
+		if (num2 == 0) {
+			return Double.NaN;
+		}
+		return resultado = num1 / num2;
 	}
 
 	/**
@@ -48,7 +50,7 @@ public class Cociente {
 	 * </p>
 	 * 
 	 * <ul>
-	 * <li>Si el divisor introducido es un 0 dará error
+	 * <li>Si el divisor introducido es un 0 devuelve NaN
 	 * <li>Si el divisor es 1 el resultado será el primer número
 	 * <li>Si el dividendo es 0, el resultado será 0
 	 * <li>Si el dividendo es infinito, devuelve infinito
@@ -63,11 +65,13 @@ public class Cociente {
 	 * @param num1 Dividendo del cociente
 	 * @param num2 Divisor del cociente
 	 * @return Devuelve el resultado de dividir num1 entre num2
-	 * @exception ArithmeticException Si num2 = 0
+	 * 
 	 */
-	public double cocienteEnteros(int num1, int num2) throws ArithmeticException {
-		double resultado = num1 / num2;
-		return resultado;
+	public double cocienteEnteros(int num1, int num2) {
+		if (num2 == 0) {
+			return Double.NaN;
+		}
+		return resultado = num1 / num2;
 	}
 
 	/**
@@ -80,17 +84,19 @@ public class Cociente {
 	 * <li>Si el número introducido es NaN, devuelve NaN
 	 * <li>Si el numero introducido es infinito, devuelve 0
 	 * <li>Si el número es infinito negativo, deuelve -0
-	 * <li>Si el número introducido es 0, salta la excepción
+	 * <li>Si el número introducido es 0, devuelve NaN
 	 * </ul>
 	 * 
 	 * @param num1 Número del que se quiere calcular el inverso
 	 * @return Devuelve un real que es el número inverso de num1
-	 * @exception ArithmeticException si num1 = 0
+	 * 
 	 */
 
 	public double calculaInverso(double num1) throws ArithmeticException {
-		double resultado = 1 / num1;
-		return resultado;
+		if (num1 == 0) {
+			return Double.NaN;
+		}
+		return resultado = 1 / num1;
 	}
 
 	/**
@@ -111,9 +117,7 @@ public class Cociente {
 	 * @return Devuelve un real que es la raiz cuadrada positiva de num1
 	 */
 	public double calculaRaiz(double num1) throws ArithmeticException {
-		double resultado = Math.sqrt(num1);
-
-		return resultado;
+		return resultado = Math.sqrt(num1);
 	}
 
 }
